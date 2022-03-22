0dddde3024f7a923676f56a925b0fff3f5a4673a
kirankumarjmeshram <85862711+kirankumarjmeshram@users.noreply.github.com>
2022-03-22 17:21:53 +0530

    Design a Parking Lot System
    A parking lot is an area where cars can be parked for a certain amount of time.
    A parking lot can have multiple floors with each floor having a different number of slots and each slot being suitable for different types of vehicles.

---
+++ parking_lot.js
@@ -0,0 +1,8 @@
+/*
+Design a Parking Lot System
+ 
+A parking lot is an area where cars can be parked for a certain amount of time.
+A parking lot can have multiple floors with each floor having a different number of slots and each slot being suitable for different types of vehicles.
+ 
+Write code making LLD decisions.
+*/
